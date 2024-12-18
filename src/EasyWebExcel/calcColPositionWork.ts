594509d00e4a94c09e53aae9926a672f8d41488c
李青 <liqingitt@gmail.com>
2024-12-18 11:26:08 +0800

    feat: 测试work线程通信

--- src/EasyWebExcel/calcColPositionWork.ts
+++ src/EasyWebExcel/calcColPositionWork.ts
@@ -1,6 +1,64 @@
-setInterval(() => {
-  self.postMessage({
-    type: 'calcColPostionWork',
-    data: 111
-  });
-}, 1000);
+import { getManualColWidthDiffTotal } from './const';
+import { Sheet } from './sheetTypes';
+
+/**
+ * 是否中断计算
+ */
+let isStop = false;
+
+/**
+ * 计算列位置
+ * @param data
+ */
+const calcColPosition = (data: Sheet) => {
+  const { maxCol, colWidth, defaultColWidth } = data;
+
+  let bufferArray: number[] = [];
+
+  /**
+   * 上一列位置
+   */
+  let lastColPosition: number | null = null;
+
+  for (let i = 0; i < maxCol; i++) {
+    // if(isStop) {
+    //   break;
+    // }
+    // bufferArray[i] = i;
+    // 上一列位置存在
+    if (lastColPosition !== null) {
+      // 上一列位置存在，计算当前列位置
+      const currentColPosition: number =
+        lastColPosition + (colWidth[i - 1] || defaultColWidth);
+      bufferArray.push(currentColPosition);
+      lastColPosition = currentColPosition;
+    } else {
+      // 上一列位置不存在，计算当前列位置
+      const manualAdjustedColIndexs: number[] = Object.keys(colWidth)
+        .map(Number)
+        .sort((a, b) => a - b);
+      const manualColWidthDiffTotal = getManualColWidthDiffTotal(
+        manualAdjustedColIndexs,
+        data,
+        i,
+      );
+
+      const currentColPosition: number =
+        i * defaultColWidth + manualColWidthDiffTotal;
+      bufferArray.push(currentColPosition);
+      lastColPosition = currentColPosition;
+    }
+    if (isStop) {
+      break;
+    }
+    if (bufferArray.length >= 1000) {
+      self.postMessage(bufferArray);
+      bufferArray = [];
+    }
+  }
+};
+
+self.onmessage = (event) => {
+  const { data } = event;
+  calcColPosition(data);
+};
--- src/EasyWebExcel/index.tsx
+++ src/EasyWebExcel/index.tsx
@@ -1,24 +1,21 @@
+import React, { useEffect, useState } from 'react';
 import { Stage } from 'react-konva';
-import { EasyWebExcelProps } from './types';
 import { GridLayer } from './GridLayer';
 import { Sheet } from './sheetTypes';
-import { useEffect, useState } from 'react';
-import React from 'react';
+import { EasyWebExcelProps } from './types';
 // TODO: 改变行高列宽渲染出问题
 // TODO: 滚动条最小时，滚到到极限时会超出
-const sheet: Sheet = {
+const sheetData: Sheet = {
   id: '1',
   name: 'Sheet1',
   maxRow: 10000,
   maxCol: 10000,
-  cells: {
-  
-  },
+  cells: {},
   rowHeight: {
     0: 100,
   },
   colWidth: {
-    2:300,
+    2: 300,
     // 10:300,
     // 100:1000,
   },
@@ -26,7 +23,7 @@ const sheet: Sheet = {
   defaultColWidth: 100,
   defaultIndexRowHeight: 30,
   defaultIndexColWidth: 50,
-}
+};
 
 const EasyWebExcel: React.FC<EasyWebExcelProps> = (props) => {
   const { size } = props;
@@ -36,33 +33,52 @@ const EasyWebExcel: React.FC<EasyWebExcelProps> = (props) => {
    */
   const [scrollDistance, setScrollDistance] = useState({
     horizontal: 0,
-    vertical: 0
+    vertical: 0,
   });
 
+  const [sheet] = useState(sheetData);
 
   useEffect(() => {
-    
-    const worker = new Worker(new URL('./calcColPositionWork',import.meta.url));
+    const worker = new Worker(
+      new URL('./calcColPositionWork', import.meta.url),
+    );
+    // worker.onmessage = (event) => {
+    //   console.log(event.data);
+    // }
+    worker.postMessage({
+      maxRow: 10000,
+      maxCol: 1000000,
+      cells: {},
+      rowHeight: {
+        0: 100,
+      },
+      colWidth: {
+        2: 300,
+        // 10:300,
+        // 100:1000,
+      },
+      defaultRowHeight: 25,
+      defaultColWidth: 100,
+    });
     worker.onmessage = (event) => {
       console.log(event.data);
-    }
+    };
   }, []);
-
 
   if (!size) {
     return null;
   }
 
- 
-
-  return <Stage width={size.width} height={size.height}>
-    <GridLayer
-      sheet={sheet}
-      size={size}
-      scrollDistance={scrollDistance}
-      setScrollDistance={setScrollDistance}
-    />
-  </Stage>
-}
+  return (
+    <Stage width={size.width} height={size.height}>
+      <GridLayer
+        sheet={sheet}
+        size={size}
+        scrollDistance={scrollDistance}
+        setScrollDistance={setScrollDistance}
+      />
+    </Stage>
+  );
+};
 
 export default EasyWebExcel;
