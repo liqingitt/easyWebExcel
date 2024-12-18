0dcda8eb17dea6f1e561170a1a72c25d3e16aef8
李青 <liqingitt@gmail.com>
2024-12-18 22:15:29 +0800

    feat: 初步打包测试成功

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,5 +2,12 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist' },
+  esm: {
+    output: 'dist',
+    ignores: ['**/demo/**'],
+  },
+
+  // extraBabelPlugins: [
+  //   ['babel-plugin-ignore-import', { pattern:['\\.demo\\.[jt]sx?$'] }]
+  // ]
 });
